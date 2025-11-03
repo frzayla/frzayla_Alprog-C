484e81f2490e2cf9e6c28145dd34db25117d0e88
Ayla_Alprog-C <frzayla@gmail.com>
2025-11-04 06:52:34 +0800

    Add files via upload

---
+++ meet2.py
@@ -0,0 +1,39 @@
+# variabel adalah cara kita menyimpan nilai
+
+# memberikan nilai\assigment
+x=10
+y =6
+print(x)
+lebar = 50
+
+print ("nilai dari x =",x)
+print("nilai dari y =",y)
+print("nilai dari lebar" ,lebar)
+
+#penamaaan variabel
+jumlah_z = 9 # jika lebih dari satu kata ,maka hanya bisa di gabung dengan undercore
+bet99 = 100000 #tidak boleh diawali angka
+jumlahZ = 10
+
+# pemanggilan kedua
+print ("nilai dari x =",x)
+x = 99
+print ("nilai dari x =",x)
+
+
+# pemanggilan indirect
+a = x
+b =lebar
+print ("nilai dari a =", a)
+print ("nilai dari b =", b) 
+
+
+
+# nama saya =(Fariza Ayla Abni Sabil)
+# nim saya =(25241072)
+
+nama = "Fariza Ayla Abni Sabil"
+nim = 25241072
+
+print("nama saya adalah",nama)
+print("nim saya adalah",nim)
