f5951531b8c6f8717f6484997a3ce7aa004c5000
Ayla_Alprog-C <frzayla@gmail.com>
2025-11-13 18:08:22 +0800

    Add files via upload

---
+++ meet4.py ayla.py
@@ -0,0 +1,39 @@
+# mengambil input dari user
+
+nama = input ("masukan nama anda ") 
+print("welcom to my room ", nama)
+print("nilai dari nama", nama)
+print(type(nama))
+
+bil_int = int(input("masuk bilangan anda"))
+print("bilangan anda adalah", bil_int)
+print("nilai dari bil_int", bil_int)
+print(type(bil_int))
+
+bil_float = float(input("masuk bilangan anda"))
+print("bilangan anda adalah", bil_float)
+print("nilai dari bil_int", bil_float)
+print(type(bil_float))
+
+bil_bool = bool(input("masuk bilangan anda"))
+print("bilangan anda adalah", bil_bool)
+print("nilai dari bil_int", bil_bool)
+print(type(bil_bool))
+
+#buatlah program dengan output sebagi berikut
+#masuka nama anda:
+#masukan umur anda:
+#nama anda adalah (nama)
+#tipe data dari nama adalah (tipe dari nama)
+#umur anda adalah (umur)
+#tipe data dari umur anda adalah (tipe dari umur)
+
+nama = input ("masukan nama anda ") 
+print("nama saya adalah ", nama)
+print("nilai dari nama saya", nama)
+print(type(nama))
+
+bil_int = int(input("masukan umur anda"))
+print("umur anda adalah", bil_int)
+print("nilai dari umur", bil_int)
+print(type(bil_int))
