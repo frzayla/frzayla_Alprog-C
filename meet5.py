7f56f2bbded24f89572f32c7180c53485be76cae
Ayla_Alprog-C <frzayla@gmail.com>
2025-11-30 20:57:11 +0800

    Add files via upload

---
+++ meet5.py
@@ -0,0 +1,32 @@
+# operasi aritmatika (x : + -)
+
+# penjumlahan 
+a = 9
+b = 3
+hasil = a + b 
+print(a, "+", b, "=", hasil)
+
+# pengurangan -
+# perkalian *
+# pembagian /
+# perpangkatan **
+a = 5**2
+b = 2**2
+hasil = a + b
+print(a, "+", b, "=", hasil)
+# modulus |
+a = 10
+b=3
+hasil = a | b 
+print(a, "modulus", b, "=", hasil)
+# floor divison //
+a = 17
+b=3
+hasil = a // b
+print(a, "floor div", b, "=", hasil)
+
+
+# operasi prioritas
+# 1. ()
+# 2. pangkat akar
+# 3. x :
